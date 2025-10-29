f8bfeaf1a499ec8ee6a33d0e47fc54f244f1c55d
DaijuKomatsu2025 <uni202506_08@telework.to>
2025-10-29 14:11:12 +0900

    アイテム２種ヒント内容更新

--- Assets/Scripts/ExtendTimer.cs
+++ Assets/Scripts/ExtendTimer.cs
@@ -2,14 +2,23 @@
 
 public class ExtendTimer : MonoBehaviour
 {
-    [SerializeField] private float _extendTime = 60f;
+    [SerializeField] private float _extendTime = 30f;
     [SerializeField] private Timer _timer;
+
+    private void Start()
+    {
+        if (_timer == null)
+        {
+            _timer = GameObject.Find("TimerText").GetComponent<Timer>();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if ((other.gameObject.tag.Contains("Player")))
         {
-            _timer.RecoverTimes();
+            if (_timer != null) _timer.RecoverTimes(_extendTime);
+
             Destroy(gameObject);
         }
     }
